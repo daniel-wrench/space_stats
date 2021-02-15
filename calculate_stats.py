86722bedb33f6af7f1f8697048658fc724934dc4
Daniel Wrench <spanner199@gmail.com>
2021-02-15 17:07:31 +1300

    Edited so can now toggle between plotting complete + gapped test data or just gapped

--- calculate_stats.py
+++ calculate_stats.py
@@ -68,17 +68,36 @@ def calc_struct_sdk(data, freq, plot):
 
 #Function for plotting observed curves against predicted for the test subsets
 
-def plot_results(predictions_arr, observed_arr, no_samples, title):
+def plot_results(predictions_arr, observed_arr, no_samples, gapped = False):
 
+    test_gapped_indices = np.load('test_gapped_indices.npy')
+    
 #    predictions = model_name.predict(test_inputs)
-    predicted = np.load(predictions_arr)
-    predicted = pd.DataFrame(predicted)
+    predictions = np.load(predictions_arr)
+    predicted = pd.DataFrame(predictions)
     predicted = predicted.transpose()
+    
+    test_pred_gapped = predictions[test_gapped_indices==1]
+    gapped_predicted = pd.DataFrame(test_pred_gapped)
+    gapped_predicted = gapped_predicted.transpose()
 
 #    observed = pd.DataFrame(test_outputs.numpy())
-    observed = np.load(observed_arr)
-    observed = pd.DataFrame(observed)
+    observations = np.load(observed_arr)
+    observed = pd.DataFrame(observations)
     observed = observed.transpose()
+    
+    test_obs_gapped = observations[test_gapped_indices==1]
+    gapped_observed = pd.DataFrame(test_obs_gapped)
+    gapped_observed = gapped_observed.transpose()
+    
+    observations_dataset = observed
+    predictions_dataset = predicted
+    title = 'Predictions from NN of test set of complete and gapped data'
+    
+    if gapped == True:
+      observations_dataset = gapped_observed
+      predictions_dataset = gapped_predicted
+      title = 'Predictions from NN of test set of only gapped data'
     
 # plots will be shown on a nplotx,nplotx grid
     if np.modf(np.sqrt(no_samples))[0] == 0:
@@ -92,8 +111,8 @@ def plot_results(predictions_arr, observed_arr, no_samples, title):
     for i in np.arange(0, no_samples):
         r, c = int(np.arange(0, no_samples)[i]/nplotx), np.mod(np.arange(0, no_samples)[i],nplotx)
         #axs[0].plot(test_inputs[i], label = "subset" + str(i+1))
-        axs[r, c].plot(observed[i], label = "observed subset" )
-        axs[r, c].plot(predicted[i], label = "predicted subset")
+        axs[r, c].plot(observations_dataset[i], label = "observed subset" )
+        axs[r, c].plot(predictions_dataset[i], label = "predicted subset")
         axs[r, c].semilogx()
         axs[r, c].semilogy()
 
@@ -103,7 +122,7 @@ def plot_results(predictions_arr, observed_arr, no_samples, title):
     # axs[0].legend()
     # axs[1].legend()
     # axs[2].legend()
-    axs[0,1].set(title =title)
+    axs[0,1].set(title = title)
     # axs[1].set(title = 'Next 20 time steps', xlabel = 'Time')
     # axs[2].set(title = 'PREDICTED next 20 time steps', xlabel = 'Time')
     #plt.title(title)
