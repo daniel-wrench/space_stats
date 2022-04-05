433b79acfe56dcef5534fe22864ecafb1a526510
Daniel Wrench <spanner199@gmail.com>
2022-04-05 15:24:06 +1200

    updating batch scripts

--- 1_batch_job.sh
+++ 1_batch_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=5G
 #SBATCH --partition=parallel
 ##SBATCH --nodelist=c01n01
 ##SBATCH --reservation=spacejam
-#SBATCH --time=12:00:00
+#SBATCH --time=00:05:00
 #SBATCH --job-name="1_read_data"
 #SBATCH -o results/1_read_data.out
 #SBATCH -e results/1_read_data.err
