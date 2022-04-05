b495c6f756c3ca7f3fe384ff9ea61c9baea9a2a3
Daniel Wrench <spanner199@gmail.com>
2022-04-06 10:25:17 +1200

    updating job submission scripts

--- 1_batch_job.sh
+++ 1_batch_job.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=5G
+#SBATCH --cpus-per-task=5
+#SBATCH --mem-per-cpu=15G
 #SBATCH --partition=parallel
 ##SBATCH --nodelist=c01n01
 ##SBATCH --reservation=spacejam
 #SBATCH --time=00:05:00
-#SBATCH --job-name="1_read_data"
+#SBATCH --job-name="1_read"
 #SBATCH -o results/1_read_data.out
 #SBATCH -e results/1_read_data.err
 
